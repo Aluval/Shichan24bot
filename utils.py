d92aa68bc7cda27cfa12b3c174b96f015ae97a83
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2023-06-21 18:05:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://kpslink.in/api'
+    url = f'https://powerlinkz.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
